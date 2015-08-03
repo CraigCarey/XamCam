f16acec2c34587c245fac664eed50fc0150e92fc
Craig Carey <sgccarey@gmail.com>
2015-08-03 16:00:56 +0100

    Displaying rear camera preview, but ratio is distorted

---
+++ XamCam.iOS/RootViewController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Foundation;
+using UIKit;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using AVFoundation;
+using CoreVideo;
+using CoreMedia;
+using CoreGraphics;
+using CoreFoundation;
+using System.Timers;
+
+namespace XamCam.iOS
+{
+    public partial class RootViewController : UIViewController
+    {
+        private NSError Error;
+        private bool Automatic = true;
+
+        public AppDelegate ThisApp
+        {
+            get { return (AppDelegate)UIApplication.SharedApplication.Delegate; }
+        }
+
+        public Timer SampleTimer { get; set; }
+        
+        static bool UserInterfaceIdiomIsPhone
+        {
+            get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
+        }
+
+        public RootViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            // Releases the view if it doesn't have a superview.
+            base.DidReceiveMemoryWarning();
+        }
+
+        #region View lifecycle
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Attach to camera view
+            ThisApp.Recorder.DisplayView = CameraView;
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+
+            // Start updating the display
+            if (ThisApp.CameraAvailable)
+            {
+                // Remap to this camera view
+                ThisApp.Recorder.DisplayView = CameraView;
+
+                ThisApp.Session.StartRunning();
+            }
+        }
+
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
+        }
+
+        public override void ViewDidDisappear(bool animated)
+        {
+            base.ViewDidDisappear(animated);
+        }
+
+        #endregion
+    }
+}
